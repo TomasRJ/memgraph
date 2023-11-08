484bbd58672aa3a99fd748bd72b93db6c767144e
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-11-08 11:22:11 +0100

    c sharp project init

---
+++ memgraph-c-sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using Neo4j.Driver;
+ 
+namespace MemgraphApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string message = "Hello, World!";
+ 
+            using var _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.None);
+            using var session = _driver.Session();
+ 
+            var greeting = session.ExecuteWrite(tx =>
+            {
+                var result = tx.Run("CREATE (n:FirstNode) " +
+                                    "SET n.message = $message " +
+                                    "RETURN 'Node '  + id(n) + ': ' + n.message",
+                    new { message });
+                return result.Single()[0].As<string>();
+            });
+            Console.WriteLine(greeting);
+        }
+    }
+}
