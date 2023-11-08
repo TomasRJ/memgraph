3618151b70355e644e31764fa781ae643d8cba8a
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-11-08 11:34:41 +0100

    use complex example

--- memgraph-c-sharp/Program.cs
+++ memgraph-c-sharp/Program.cs
@@ -2,24 +2,42 @@
  
 namespace MemgraphApp
 {
-    public class Program
+  public class Program : IDisposable
+  {
+    private readonly IDriver _driver;
+ 
+    public Program(string uri, string user, string password)
     {
-        public static void Main()
-        {
-            string message = "Hello, World!";
- 
-            using var _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.None);
-            using var session = _driver.Session();
- 
-            var greeting = session.ExecuteWrite(tx =>
-            {
-                var result = tx.Run("CREATE (n:FirstNode) " +
-                                    "SET n.message = $message " +
-                                    "RETURN 'Node '  + id(n) + ': ' + n.message",
-                    new { message });
-                return result.Single()[0].As<string>();
-            });
-            Console.WriteLine(greeting);
-        }
+      _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
     }
+ 
+    public void PrintGreeting(string message)
+    {
+      using (var session = _driver.Session())
+      {
+        var greeting = session.ExecuteWrite(tx =>
+        {
+          var result = tx.Run("CREATE (n:FirstNode) " +
+                    "SET n.message = $message " +
+                    "RETURN 'Node '  + id(n) + ': ' + n.message",
+            new { message });
+          return result.Single()[0].As<string>();
+        });
+        Console.WriteLine(greeting);
+      }
+    }
+ 
+    public void Dispose()
+    {
+      _driver?.Dispose();
+    }
+ 
+    public static void Main()
+    {
+      using (var greeter = new Program("bolt://localhost:7687", "", ""))
+      {
+        greeter.PrintGreeting("Hello, World!");
+      }
+    }
+  }
 }
