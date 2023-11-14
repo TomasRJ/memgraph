bf6003cda99ddac581463272357ca9b1ea565934
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-11-14 18:39:36 +0100

    c sharp project code refactor

--- memgraph-c-sharp/HelloWorld.cs
+++ memgraph-c-sharp/HelloWorld.cs
@@ -1,43 +1,38 @@
 ﻿using Neo4j.Driver;
- 
-namespace MemgraphApp
+
+namespace memgraph_c_sharp;
+
+public class HelloWorld : IDisposable
 {
-  public class HelloWorld : IDisposable
+  private readonly IDriver _driver;
+
+  private HelloWorld(string uri, string user, string password)
   {
-    private readonly IDriver _driver;
- 
-    public HelloWorld(string uri, string user, string password)
+    _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+  }
+
+  private void PrintGreeting(string message)
+  {
+    using var session = _driver.Session();
+    var greeting = session.ExecuteWrite(tx =>
     {
-      _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
-    }
- 
-    public void PrintGreeting(string message)
-    {
-      using (var session = _driver.Session())
-      {
-        var greeting = session.ExecuteWrite(tx =>
-        {
-          var result = tx.Run("CREATE (n:FirstNode) " +
-                    "SET n.message = $message " +
-                    "RETURN 'Node '  + id(n) + ': ' + n.message",
-            new { message });
-          return result.Single()[0].As<string>();
-        });
-        Console.WriteLine(greeting);
-      }
-    }
- 
-    public void Dispose()
-    {
-      _driver?.Dispose();
-    }
- 
-    public static void Main()
-    {
-      using (var greeter = new HelloWorld("bolt://localhost:7687", "", ""))
-      {
-        greeter.PrintGreeting("Hello World!");
-      }
-    }
+      var result = tx.Run("CREATE (n:FirstNode) " +
+                          "SET n.message = $message " +
+                          "RETURN 'Node '  + id(n) + ': ' + n.message",
+        new { message });
+      return result.Single()[0].As<string>();
+    });
+    Console.WriteLine(greeting);
+  }
+
+  public void Dispose()
+  {
+    _driver.Dispose();
+  }
+
+  public static void Main()
+  {
+    using var greeter = new HelloWorld("bolt://localhost:7687", "", "");
+    greeter.PrintGreeting("Hello World!");
   }
 }
